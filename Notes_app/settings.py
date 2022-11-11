562092cbf894dc98e47a3f5864e706a948e9c4d6
candykarthi002 <candykarthi2004.gd@gmail.com>
2022-11-11 09:37:19 +0530

    Updated STATIC_ROOT

--- Notes_app/settings.py
+++ Notes_app/settings.py
@@ -119,6 +119,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.join.path(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 LOGIN_URL = 'login/'
